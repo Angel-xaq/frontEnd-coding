93f0edb32bdd0ca68194d56196a2c0c10e97b63d
Angel <1210772173@qq.com>
2023-07-13 16:51:51 +0800

    first

--- my/防抖函数.js
+++ my/防抖函数.js
@@ -7,3 +7,16 @@ var decounce=(fn,delay)=>{
     },delay)
   }
 }
+//这个是学姐的，比较简洁的非立即执行防抖函数
+
+//我的防抖
+function debounce(func, wait) {  
+let timeout;   //这个只会在首次调用的时候执行
+return function () {
+    const args = arguments;
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        func.apply(this, args)   //调用间隔小于wait的时候不执行
+    }, wait);  
+}
+}  
