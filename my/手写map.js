26313de39d26649711af37bbfd56b7a1fdd92fda
Angel <1210772173@qq.com>
2023-08-21 23:55:34 +0800

    first

--- my/手写map.js
+++ my/手写map.js
@@ -1,6 +1,6 @@
 var arr=[1,2,3]
 Array.prototype.mymap = function(callback, thisArg){
-    let length = this.length;
+  let length = this.length;
   let result = [];
   if (!Array.isArray(this)) throw new TypeError('this is not an array'); //如果不是数组调用返回错误
   if (typeof callback !== 'function') throw new TypeError(callback + 'is not a function');  //回调方法必须是个函数
